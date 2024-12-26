1fb98e2435441b246282c7051ed21bed718f65b5
yasmalhotraa <yashmalhotra330@gmail.com>
2024-12-26 17:35:58 +0530

    adding dynamic port

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("server running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
